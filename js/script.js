981afc9a3f3f2069283480a7f2407669320ee613
jbiers <juliabiersuriano@gmail.com>
2021-12-16 11:44:51 -0400

    bug to fix

--- js/script.js
+++ js/script.js
@@ -17,36 +17,75 @@ function userPlay() {
 // scissors > paper
 
 // compares user and computer play
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, playerScore, computerScore) {
     if (playerSelection == computerSelection) {
         return 'You have a tie!'
     }
 
     // Rock possibilities beyond a tie
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore = playerScore + 1
         return 'You win! Rock beats Scissors.'
     }
 
     if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore = computerScore + 1
         return 'You lose! Paper beats Rock.'
     }
 
 
     // Paper possibilities beyond a tie
     if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore = computerScore + 1
         return 'You lose! Scissors beat Paper'
     }
 
     if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore = playerScore + 1
         return 'You win! Paper beats Rock.'
     }
 
     // Scissors possibilities beyond a tie
     if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerScore = computerScore + 1
         return 'You lose! Rock beats Scissors.'
     }
 
     if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore = playerScore + 1
         return 'You win! Scissors beats Paper.'
     }
 }
+
+// this is the gameloop function
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+
+    let playerSelection
+    let computerSelection
+
+    for (let i = 0; i < 5; i = i + 1) {
+
+        playerSelection = userPlay()
+        computerSelection = computerPlay()
+
+        console.log(playRound(playerSelection, computerSelection, playerScore, computerScore))
+    }
+
+    if (playerScore > computerScore) {
+        console.log('You win the game!')
+    }
+
+    if (playerScore < computerScore) {
+        console.log('You lose the game!')
+    }
+
+    if (playerScore == computerScore) {
+        console.log('Game ended in a tie!')
+    }
+}
+
+game()
+
+// fix the playerScore and computerScore
